e20156478f34140f25437fe7a5d78ba87d1576ee
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-08-11 16:40:30 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 146 ms, Memory: 30,828 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -2,7 +2,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -10,6 +11,7 @@ public class Solution {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int[][] ladder = new int[100][100];
+		List<Integer> list = new ArrayList<>();
 
 		for (int i = 1; i <= 10; i++) {
 			int T = Integer.parseInt(br.readLine());
@@ -23,48 +25,47 @@ public class Solution {
 			// 도착지점 찾기
 			int endPoint = 0;
 			for (int col = 0; col < 100; col++) {
-				if (ladder[99][col] == 2) {
+				if(ladder[0][col] == 1) { // 출발지점 후보군
+					list.add(col);
+				} 
+				if (ladder[99][col] == 2) { // 도착지점
 					endPoint = col;
-					break;
 				}
+			}
+			// 도착지점의 사다리가 후보군 리스트의 몇번째 인덱스인지?
+			int endColIdx = 0;
+			for(int j = 0; j < list.size(); j++) {
+				if(list.get(j) == endPoint)
+					endColIdx = j;
 			}
 			// 도착지점으로부터 위로 올라가면서 출발지점을 찾는다.
 			int startPoint;
 			int curRow = 99;
 			int curCol = endPoint;
-			int[] dr = { 0, 0, -1 };
-			int[] dc = { -1, 1, 0 };
-			while (true) {
 
-				for (int j = 0; j < 3; j++) {
-					int nr = curRow + dr[j];
-					int nc = curCol + dc[j];
-					if (nc >= 0 && nc <= 99 && nr >= 0 && ladder[nr][nc] == 1) {
-						curRow = nr;
-						curCol = nc;
-						ladder[curRow][curCol] = 0;
-						break;
-					}
+			while (true) {
+				// 1. 왼쪽
+				if (curCol != 0 && ladder[curRow][curCol - 1] == 1) {
+					endColIdx -= 1;
+					curRow -= 1;
+					curCol = list.get(endColIdx);
 				}
-//			// 1. 왼쪽
-//			if (curCol != 0 && ladder[curRow][curCol - 1] == 1) {
-//				curCol -= 1;
-//			}
-//			// 2. 오른쪽
-//			else if (curCol != 99 && ladder[curRow][curCol + 1] == 1) {
-//				curCol += 1;
-//			}
-//			// 3. 위
-//			else if (curRow != 0 && ladder[curRow - 1][curCol] == 1) {
-//				curRow -= 1;
-//			}
-//			ladder[curRow][curCol] = 0;
+				// 2. 오른쪽
+				else if (curCol != 99 && ladder[curRow][curCol + 1] == 1) {
+					endColIdx += 1;
+					curRow -= 1;
+					curCol = list.get(endColIdx);
+				}
+				// 3. 위
+				else {
+					curRow -= 1;
+				}
+
 				if (curRow == 0) {
-					startPoint = curCol;
+					System.out.println("#" + i + " " + list.get(endColIdx));
 					break;
 				}
 			}
-			System.out.println("#" + i + " " + startPoint);
 		}
 	}
 }
