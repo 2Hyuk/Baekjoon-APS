681347e0d0806403ea91c4932631b39f423d286b
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-08-12 10:48:05 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 992 ms, Memory: 252532 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		for(int testCase = 0; testCase < T; testCase++) {
+			int N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			int max = Integer.MIN_VALUE;
+			int min = Integer.MAX_VALUE;
+			for(int i = 0; i < N; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				max = Math.max(max, num);
+				min = Math.min(min, num);
+			}
+			System.out.println(min + " " + max);
+		}
+	}
+}
