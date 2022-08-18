72dd9358d94f35fb06ecd7a1c3473e4d406fdcf7
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-08-18 11:20:47 +0900

    [Silver IV] Title: 카드2, Time: 232 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -6,21 +6,34 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		Queue<Integer> queue = new LinkedList<>();
-		
-		//큐에 1~N까지 추가
-		for(int i = 1; i <= N; i++) {
-			queue.offer(i);
-		}
-		
-		while(queue.size() > 1) {
-			//첫번째 원소 뺴고
-			queue.poll();
-			//두번째원소는 제일뒤로 넣음
-			queue.offer(queue.poll());
-			
+		int result = 1;
+		for(int i = 0;; i++) {
+			if(N == Math.pow(2, i)) {
+				result = N; 
+				break;
 			}
-			//큐에 1개의 원소가 있을 때
-			System.out.println(queue.poll());
+			if(N < Math.pow(2, i)) {
+				result = (int) (2 * (N - Math.pow(2, i-1)));
+				break;
+			}
+		}
+		System.out.println(result);
+		//Queue 사용
+//		Queue<Integer> queue = new LinkedList<>();
+//		
+//		//큐에 1~N까지 추가
+//		for(int i = 1; i <= N; i++) {
+//			queue.offer(i);
+//		}
+//		
+//		while(queue.size() > 1) {
+//			//첫번째 원소 뺴고
+//			queue.poll();
+//			//두번째원소는 제일뒤로 넣음
+//			queue.offer(queue.poll());
+//			
+//			}
+//			//큐에 1개의 원소가 있을 때
+//			System.out.println(queue.poll());
 	}
 }
