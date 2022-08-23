e28951e644f93eb4a714b24a76d0c82c61bbc830
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-08-23 11:51:20 +0900

    [Bronze IV] Title: !밀비 급일, Time: 120 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String str = br.readLine();
+			if(str.equals("END")) break;
+			System.out.println(sb.append(str).reverse());
+			sb.setLength(0);
+		}
+	}
+}
