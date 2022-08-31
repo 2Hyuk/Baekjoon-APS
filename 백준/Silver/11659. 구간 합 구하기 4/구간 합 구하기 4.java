b4048d412bb4c4ec24e103a02fede8887d4486c1
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-08-31 22:28:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1520 ms, Memory: 58712 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,26 +1,32 @@
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	/*
 	 * 값을 입력받으면서 누적합 배열을 만든다.
 	 * 이 후 i번째에서 j번째 합을 구하라고 했을 때 sumArr[j-1] - sumArr[i-2]를 하게 되면 답이 나온다.
 	 * */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		int[] sumArr = new int[N];
 		//초기값
-		sumArr[0] = sc.nextInt();
+		st = new StringTokenizer(br.readLine());
+		sumArr[0] = Integer.parseInt(st.nextToken());
 		for(int i = 1; i < N; i++) {
-			sumArr[i] += sumArr[i - 1] + sc.nextInt();
+			sumArr[i] += sumArr[i - 1] + Integer.parseInt(st.nextToken());
 		}
 		
 		for(int k = 0; k < M; k++) {
-			int i = sc.nextInt();
-			int j = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
 			int sum = 0;
 			//i 가 1이라면  1부터 포함되므로 처음부터 j까지 누적한다.
 			if(i == 1) {
