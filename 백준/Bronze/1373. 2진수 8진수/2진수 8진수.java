37f5264e841c03699f234f530cca81b151ce16ed
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-09-12 21:28:54 +0900

    [Bronze I] Title: 2진수 8진수, Time: 396 ms, Memory: 26424 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -8,10 +8,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringBuilder result = new StringBuilder();
+		//2진수를 String 으로 받아서 3의 배수개로 맞춰준다.
 		sb.append(br.readLine()).reverse();
 		while(sb.length() % 3 != 0) {
 			sb.append("0");
 		}
+		//3개씩 끊은 후, 하나씩 보면서 1인 경우에는 자리별로 2^0, 2^1, 2^2를 더해준다.
 		String bin = sb.toString();
 		for(int i = 0; i < bin.length(); i += 3) {
 			int sum = 0;
@@ -22,6 +24,7 @@ public class Main {
 			}
 			result.append(sum);
 		}
+		// 다시 뒤집어준다.
 		System.out.println(result.reverse());
 	}
 }
