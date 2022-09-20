8070001e711861e5fc8ebde8fe8084c881df3272
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-09-20 11:47:49 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -5,26 +6,58 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	static int[] arr = new int[9];
+	static boolean[] ch = new boolean[9];
+	static List<Integer> list = new ArrayList<>();
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		List<Integer> list = new ArrayList<>();
-		int sum = 0;
-		for(int i = 0; i < 9; i++) {
-			list.add(sc.nextInt());
-			sum += list.get(i);
+		for (int i = 0; i < 9; i++) {
+			arr[i] = sc.nextInt();
 		}
+		list.clear();
+		combi(0, 0, 0);
 		Collections.sort(list);
-		loop:for(int i = 0; i < list.size()-1; i++) {
-			for(int j = i + 1; j < list.size(); j++) {
-				if(sum - (list.get(i) + list.get(j)) == 100) {
-					list.remove(j);
-					list.remove(i);
-					break loop;
-				}
-			}
-		}
-		for(int i : list) {
+//		List<Integer> list = new ArrayList<>();
+//		int sum = 0;
+//		for(int i = 0; i < 9; i++) {
+//			list.add(sc.nextInt());
+//			sum += list.get(i);
+//		}
+//		Collections.sort(list);
+//		loop:for(int i = 0; i < list.size()-1; i++) {
+//			for(int j = i + 1; j < list.size(); j++) {
+//				if(sum - (list.get(i) + list.get(j)) == 100) {
+//					list.remove(j);
+//					list.remove(i);
+//					break loop;
+//				}
+//			}
+//		}
+		for (int i : list) {
 			System.out.println(i);
+		}
+
+	}
+	static boolean check = false;
+	public static void combi(int idx, int sidx, int sum) {
+		if(check)
+			return;
+		if (sidx == 7) {
+			if (sum == 100) {
+				for (int i = 0; i < ch.length; i++) {
+					if (ch[i] == true)
+						list.add(arr[i]);
+				}
+				check = true;
+			}
+			return;
+		}
+
+		for (int i = idx; i <= 2 + sidx; i++) {
+			ch[i] = true;
+			combi(i + 1, sidx + 1, sum + arr[i]);
+			ch[i] = false;
 		}
 	}
 }
