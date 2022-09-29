80752b85ee65b060360106460a3212e60eda4846
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-09-29 16:17:54 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 284 ms, Memory: 24924 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,55 +1,91 @@
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
+	static class Move implements Comparable<Move>{
+		int x, time;
 
-	static int N;
-	static int K;
-	static Queue<int[]> queue = new LinkedList<>();
-	static int[] visited = new int[100001];
+		public Move(int x, int time) {
+			this.x = x;
+			this.time = time;
+		}
 
+		@Override
+		public int compareTo(Move o) {
+			return this.time - o.time;
+		}
+		
+		
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-
-		visited[N] = 1;
-		queue.offer(new int[] { N, 1 });
-		while (!queue.isEmpty()) {
-			int[] loc = queue.poll();
-			bfs(loc[0], loc[1]);
-		}
-
-		System.out.println(visited[K] - 1);
-	}
-
-	static void bfs(int point, int time) {
-
-		int[] nextPoints = setPoint(point);
-		for (int i = 0; i < 3; i++) {
-			int nextPoint = nextPoints[i];
-			if (nextPoint >= 0 && nextPoint <= 100000) {
-				if (i == 2) {
-					if (visited[nextPoint] == 0 || visited[nextPoint] > time) {
-						visited[nextPoint] = time;
-						queue.offer(new int[] { nextPoint, time });
-					}
-				} 
-				else {
-					if (visited[nextPoint] == 0 || visited[nextPoint] > time + 1) {
-						visited[nextPoint] = time + 1;
-						queue.offer(new int[] { nextPoint, time + 1 });
-
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		//boolean[] visited = new boolean[100001];
+		int[] dist = new int[100001];
+		
+		final int INF = Integer.MAX_VALUE;
+		Arrays.fill(dist, INF);
+		
+		PriorityQueue<Move> pq = new PriorityQueue<>();
+		dist[N] = 0;
+		pq.offer(new Move(N, dist[N]));
+		
+		boolean[] visited = new boolean[100001];
+		
+		while(!pq.isEmpty()) {
+			Move m = pq.poll();
+			
+			if(m.x == K) {
+				System.out.println(m.time);
+				break;
+			}
+			
+			if(visited[m.x]) continue;
+			
+			visited[m.x] = true;
+			
+			int[] movePoint = movePoint(m.x);
+			for(int i = 0; i < 3; i++) {
+				int x = movePoint[i];
+				if(x < 0 || x > 100000) continue;
+				if(i == 2) {
+					if(!visited[x] && dist[x] > dist[m.x]) {
+						dist[x] = dist[m.x];
+						pq.offer(new Move(x, dist[x]));
 					}
 				}
-
+				else {
+					if(!visited[x] && dist[x] > dist[m.x] + 1) {
+						dist[x] = dist[m.x] + 1;
+						pq.offer(new Move(x, dist[x]));
+					}
+				}
 			}
 		}
-	}
 
-	static int[] setPoint(int point) {
-		return new int[] { point + 1, point - 1, point * 2 };
+	}
+	
+	static int[] movePoint(int x) {
+		return new int[] {x-1, x+1, 2*x};
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
