3ef95262b2be3607edf87f6a18203a740bb9fb48
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-10-04 18:04:42 +0900

    [Silver I] Title: 숨바꼭질, Time: 256 ms, Memory: 26288 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -4,57 +4,64 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static class Node{
+	static class Node {
 		int x;
 		int time;
+
 		public Node(int x, int time) {
 			this.x = x;
 			this.time = time;
 		}
 	}
+
 	static int N;
 	static int K;
 	static int answer;
 	static Queue<Node> queue = new LinkedList<>();
 	static boolean[] visited = new boolean[100001];
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 		K = sc.nextInt();
-		
+		// 같은 지점에 있을 떄 0이 출력되야되므로 초기값으로 -1을 주었다.
 		answer = -1;
+		// 시작점을 체크하고 시작한다.
 		visited[N] = true;
 		bfs(N, 0);
 		System.out.println(answer);
 	}
-	
+
 	static void bfs(int point, int time) {
-		if(answer != -1) return;
+		queue.offer(new Node(point, 0));
 		
-		if(answer == -1 && point == K) {
-			answer = time;
-		}
-		int nextPoint = point + 1;
-		if(nextPoint >= 0 && nextPoint <= 100000 && visited[nextPoint] == false) {
-			visited[nextPoint] = true;
-			queue.offer(new Node(nextPoint, time));
-		}
-		
-		nextPoint = point - 1;
-		if(nextPoint >= 0 && nextPoint <= 100000 && visited[nextPoint] == false) {
-			visited[nextPoint] = true;
-			queue.offer(new Node(nextPoint, time));
-		}
-		
-		nextPoint = 2 * point;
-		if(nextPoint >= 0 && nextPoint <= 100000 && visited[nextPoint] == false) {
-			visited[nextPoint] = true;
-			queue.offer(new Node(nextPoint, time));
-		}
-		
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			Node node = queue.poll();
-			bfs(node.x, node.time + 1);
+			// 정답을 찾았으니 종료
+			if (answer != -1)
+				return;
+
+			// K에 도착했으니 종료
+			if (answer == -1 && node.x == K) {
+				answer = node.time;
+				return;
+			}
+
+			// 이동할 좌표를 배열로 받아온다.
+			int[] nextPoints = setPoint(node.x);
+			for (int i = 0; i < 3; i++) {
+				int nextPoint = nextPoints[i];
+				// 이동할 좌표가 0~100000 사이에 있고 방문한 적이 없다면 갈 수 있다.
+				if (nextPoint >= 0 && nextPoint <= 100000 && visited[nextPoint] == false) {
+					visited[nextPoint] = true;
+					queue.offer(new Node(nextPoint, node.time + 1));
+				}
+			}
 		}
+
+	}
+
+	static int[] setPoint(int point) {
+		return new int[] { point + 1, point - 1, point * 2 };
 	}
 }
