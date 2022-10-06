1048a18f54827c977a81ee80f362b7ad4679839d
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-10-06 22:27:00 +0900

    [Gold III] Title: 당근 훔쳐 먹기, Time: 656 ms, Memory: 68816 KB -BaekjoonHub

---
+++ 백준/Gold/18234. 당근 훔쳐 먹기/당근 훔쳐 먹기.java
@@ -0,0 +1,52 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Carrot implements Comparable<Carrot> {
+		int w, p;
+
+		public Carrot(int w, int p) {
+			super();
+			this.w = w;
+			this.p = p;
+		}
+
+		@Override
+		public int compareTo(Carrot o) {
+			return o.p - this.p;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		Set<Integer> set = new HashSet<>();
+		List<Carrot> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			list.add(new Carrot(w, p));
+
+		}
+		long result = 0;
+		Collections.sort(list);
+		for(int i = T-N; i < T; i++) {
+			Carrot carrot = list.get(list.size() - 1);
+			result += (long)( carrot.w + (long)(carrot.p * (long)i));
+			list.remove(list.size() - 1);
+		}
+		System.out.println(result);
+
+	}
+}
