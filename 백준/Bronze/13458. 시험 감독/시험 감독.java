9f8e80fa66acde92da14f9610f719212bfd74399
JH Lee <94176133+2Hyuk@users.noreply.github.com>
2022-11-01 09:10:49 +0900

    [Bronze II] Title: 시험 감독, Time: 1680 ms, Memory: 318168 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.java
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -1,29 +1,30 @@
-
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+        //시험장의 수
 		int[] area = new int[N];
 		for (int i = 0; i < N; i++) {
+            //각 시험장의 사람 수
 			area[i] = sc.nextInt();
 		}
-
-		int B = sc.nextInt();
-		double C = sc.nextDouble();
+        
+		int B = sc.nextInt(); // 감독
+		double C = sc.nextDouble();//부감독
 
 		long ans = 0;
 
 		for (int i = 0; i < N; i++) {
-			area[i] -= B;
-			ans++;
-			if (area[i] > 0) {
-				if(area[i] / C == 0) {
+			area[i] -= B; //감독이 케어할 수 있는만큼 빼준다
+			ans++; //감독수 추가
+			if (area[i] > 0) { // 그래도 사람이 남아있따면
+				if(area[i] / C == 0) { // 한사람으로 커버되면 한명만
 					ans++;
 				}
 				else {
-					ans += Math.ceil(area[i] / C);
+					ans += Math.ceil(area[i] / C); //아니면 더 추가
 				}
 			}
 		}
