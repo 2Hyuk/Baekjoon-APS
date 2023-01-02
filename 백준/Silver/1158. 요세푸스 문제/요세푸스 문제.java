6982817566850a5108931785485d133da4ba2bdb
BinaryH <94176133+BinaryHyuk@users.noreply.github.com>
2023-01-02 15:43:29 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 344 ms, Memory: 19768 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,31 +1,34 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		StringBuilder sb = new StringBuilder();
-		//Queue
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 1; i <= N; i++)
-			queue.offer(i);
-		
-		sb.append("<");
-		int cycle = 0;
-		while(queue.size() != 1) {
-			cycle++;
-			if(cycle == K) {
-				sb.append(queue.poll()).append(", ");
-				cycle = 0;
-			}
-			else {
-				queue.offer(queue.poll());
-			}
-		}
-		sb.append(queue.poll()).append(">");
-		System.out.println(sb);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+
+        int cycle = 0;
+        while(queue.size() != 1){
+            cycle++;
+            if(cycle == K){
+                sb.append(queue.poll()).append(", ");
+                cycle = 0;
+            }
+            else{
+                queue.add(queue.poll());
+            }
+        }
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
+    }
 }
