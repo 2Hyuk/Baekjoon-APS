4d5f9144afbe0d25ba13293d72509d26e6e79263
Binary_hyeok <94176133+BinaryHyeok@users.noreply.github.com>
2023-09-20 18:25:22 +0900

    [level 2] Title: [1차] 캐시, Time: 1.79 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,24 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
+        
+        if(cacheSize == 0)
+            return cities.length * 5;
         int answer = 0;
+        
         LinkedList<String> list = new LinkedList<>();
         
         for(String city : cities){
             String cityName = city.toLowerCase();
             int idx = list.indexOf(cityName);
-            if(idx < cacheSize && idx >= 0){
+          
+            if(idx >= 0){
                 answer += 1;
+                list.remove(idx);
             }
             else{
                 answer += 5;
+                if(list.size() >= cacheSize){
+                    list.remove(0);
+                }
             }
-            if(list.contains(cityName)){
-                list.remove(idx);
-            }
-            list.addFirst(cityName);
+            list.offer(cityName);
         }
-        
         return answer;
     }
 }
